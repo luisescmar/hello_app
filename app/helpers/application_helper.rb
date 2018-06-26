a7681e53aab45ea7c35a2dafc4d308a77ebe7b1a
luisescmar <luisescmar@gmail.com>
2018-06-26 18:14:31 +0200

    Chapter 4 Intermediate

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
+  def kakafuti()
+    list = { one: "uno", two: "dos", three: "tres" }
+    list.each do |key, value|
+      puts "Key #{key.inspect} has Spanish is #{value.inspect}"
+    end
+  end
+
+  def parents()
+    person1 = {first: "Pepe", last: "Pepe"}
+    person2 = {first: "Manoli", last: "Manoli"}
+    person3 = {first: "Juanito", last: "Juanito"}
+    #params = {father: person1, mother: person2, child: person3}
+    params[:father] = person1
+    params[:mother] = person2
+    params[:child] = person3
+
+    params[:father][:first]
 end
