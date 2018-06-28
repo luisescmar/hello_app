f82898b1fd0b5f8301d5c1ff34e9bad479ee137e
lescuder <lescuder@everis.com>
2018-06-28 08:50:15 +0200

    Chapter 5 finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
     has_many :microposts
-    validates :name, presence:true
-    validates :email, presence:true
+    validates :name, presence:true,
+                    length: {maximum: 50}
+    validates :email, presence:true,
+                    length: {maximum: 255},
+                    format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i},
+                    uniqueness: {case_sensitive: false}
+    #validates :password, length: {minimum: 6}
+    before_save {self.email = email.downcase}
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
 end
