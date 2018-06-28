bc29fbe6b6e7fc25e61b9d9058b74778e59a6482
lescuder <lescuder@everis.com>
2018-06-28 14:29:16 +0200

    Chapter  finished

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_27_163027) do
+ActiveRecord::Schema.define(version: 2018_06_28_114759) do
 
   create_table "microposts", force: :cascade do |t|
     t.string "content"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_06_27_163027) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
