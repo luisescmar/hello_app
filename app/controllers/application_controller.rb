54fd96870ff3b2c93ce75259b784ef6cb83451b7
lescuder <lescuder@everis.com>
2018-06-28 13:27:15 +0200

    Chapter 8 finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
 
   def hello
     render html: "¡Odio los Hola Mundo! ¡Únete a mi causa!"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
@@ -16,4 +17,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
